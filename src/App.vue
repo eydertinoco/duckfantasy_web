d11a34765122e6f636bef82be4c11fd3ac04bc11
eyder <eydertinoco@outlook.com>
2023-03-10 13:46:00 -0300

    - Removido página AboutView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
     <router-link to="/login">Login</router-link> |
-    <router-link to="/user">User</router-link>
+    <router-link to="/user">Minha Página</router-link>
   </nav>
   <router-view/>
 </template>
