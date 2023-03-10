080a8652676e682bbe1c68bde929fe9401786d53
eyder <eydertinoco@outlook.com>
2023-03-10 13:15:16 -0300

    - removido primevue;

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-import PrimeVue from 'primevue/config';
 
 import './assets/scss/app.scss';
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).use(PrimeVue).mount('#app');
+createApp(App).use(router).use(pinia).mount('#app');
